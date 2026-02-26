3a272f7dff5fc49fbada6baca524e5e59d5cce32
achraf abdelhakim essaloua <abdalhakimgo@gmail.com>
2026-02-26 14:12:58 +0000

    feat(invitation): define relationship with colocation and mass assignment rules

---
+++ app/Models/Invitation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invitation extends Model
+{
+    protected $fillable = [
+        'colocation_id',
+        'email',
+        'token',
+        'status',
+        'expires_at',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+}
