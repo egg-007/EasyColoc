483c0c16da6c0120c58d76a2dde9d4495e9a1179
achraf abdelhakim essaloua <abdalhakimgo@gmail.com>
2026-03-02 15:33:47 +0000

    feat(expense): register expense creation route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\MembershipController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,7 +26,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/invitations/{token}/accept', [InvitationController::class, 'accept'])->name('invitations.accept');
     Route::get('/invitations/{token}/refuse', [InvitationController::class, 'refuse'])->name('invitations.refuse');
     Route::resource('colocations', ColocationController::class);
+    Route::post('/colocations/{colocation}/payments',[PaymentController::class, 'store'])->name('payments.store');
+    
+    // Nouveaux ajouts :
+    Route::post('/colocations/{colocation}/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    Route::post('/colocations/{colocation}/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+    Route::delete('/memberships/{membership}', [MembershipController::class, 'destroy'])->name('memberships.destroy');
 
+    // Admin routes
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::patch('/admin/users/{user}/ban', [AdminController::class, 'ban'])->name('admin.users.ban');
+    Route::patch('/admin/users/{user}/unban', [AdminController::class, 'unban'])->name('admin.users.unban');
 });
 
 require __DIR__.'/auth.php';
